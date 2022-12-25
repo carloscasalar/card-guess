091218905276659cb9010f2d5e6afb7ac5233fcf
Carlos Castillo <castillo.st@gmail.com>
2022-12-25 17:28:22 +0100

    🩹 Fix goimport on some files

--- cmd/card-guess/main.go
+++ cmd/card-guess/main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
-	"github.com/carloscasalar/card-guess/internal/deck"
 	"os"
+
+	"github.com/carloscasalar/card-guess/internal/deck"
 )
 
 const TrickSampleSize = 21
--- internal/deck/pile_test.go
+++ internal/deck/pile_test.go
@@ -1,11 +1,12 @@
 package deck_test
 
 import (
+	"testing"
+
 	"github.com/carloscasalar/card-guess/internal/deck"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
-	"testing"
 )
 
 var (
