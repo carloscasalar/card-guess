08f19b533b7a1d138b129881414c7781747c2437
Carlos Castillo <castillo.st@gmail.com>
2022-12-25 20:04:36 +0100

    🚧 Modify main to show the mat and ask for the user to say the pile

--- cmd/card-guess/main.go
+++ cmd/card-guess/main.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/manifoldco/promptui"
+
+	"github.com/carloscasalar/card-guess/internal/trick"
+
 	"github.com/carloscasalar/card-guess/internal/deck"
 )
 
@@ -21,17 +25,47 @@ func run() error {
 
 	dealer.ShuffleCards()
 
-	cards := make([]deck.Card, TrickSampleSize)
-	for i := 0; i < 21; i++ {
+	sample := deck.NewPile()
+	for i := 0; i < TrickSampleSize; i++ {
 		card, err := dealer.Deal()
 		if err != nil {
 			return fmt.Errorf("unexpected error while dealing the card %vth: %w", i+1, err)
 		}
-		cards[i] = card
+		sample = sample.AddCard(card)
 	}
 
-	fmt.Println(cards)
-	fmt.Println("Pick a card from above and hold it in your mind, then press enter.")
+	fmt.Println(sample.String())
+	fmt.Println("Pick a card from above and hold it in your mind.")
+	fmt.Println("Now I'll split the cards into three piles, watch your card.")
+	fmt.Println("Then press enter to continue")
+	_, _ = fmt.Scanln()
+
+	mat := trick.NewMat()
+	for {
+		var card deck.Card
+		var err error
+		card, sample, err = sample.DrawCard()
+		if err != nil {
+			if err == deck.ErrNoMoreCardsInThePile {
+				break
+			}
+			return err
+		}
+		mat.PlaceIntoNextPile(card)
+	}
+
+	prompt := promptui.Select{
+		Label: "Select the pile where your card is",
+		Items: mat.Piles(),
+	}
+
+	_, result, err := prompt.Run()
+
+	if err != nil {
+		return fmt.Errorf("Prompt failed %w\n", err)
+	}
+
+	fmt.Printf("You choose %q\n", result)
 
 	return nil
 }
--- go.mod
+++ go.mod
@@ -4,11 +4,14 @@ go 1.19
 
 require (
 	github.com/carloscasalar/go-cards v1.0.0
+	github.com/manifoldco/promptui v0.9.0
 	github.com/stretchr/testify v1.8.1
 )
 
 require (
+	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20181122145206-62eef0e2fa9b // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
