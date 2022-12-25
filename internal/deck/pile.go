8c8f55964a20352a7121883b44eccd8bacfa3541
Carlos Castillo <castillo.st@gmail.com>
2022-12-25 14:50:21 +0100

    ✅ Implement one card piles

--- internal/deck/pile.go
+++ internal/deck/pile.go
@@ -1,11 +1,21 @@
 package deck
 
-type Pile struct{}
+type Pile struct {
+	card *Card
+}
 
 func (p *Pile) DrawCard() (*Card, error) {
+	if p.card != nil {
+		card := p.card
+		p.card = nil
+		return card, nil
+	}
 	return nil, NoMoreCardsInThePile
 }
 
-func NewPile() *Pile {
-	return &Pile{}
+func NewPile(cards ...Card) *Pile {
+	if len(cards) == 0 {
+		return &Pile{}
+	}
+	return &Pile{&cards[0]}
 }
--- internal/deck/pile_test.go
+++ internal/deck/pile_test.go
@@ -3,6 +3,7 @@ package deck_test
 import (
 	"github.com/carloscasalar/card-guess/internal/deck"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
@@ -12,4 +13,35 @@ func TestDrawCard_should_return_error_when_there_is_no_cards_in_the_pile(t *test
 	_, err := pile.DrawCard()
 
 	assert.ErrorIs(t, err, deck.NoMoreCardsInThePile)
+}
+
+func TestDrawCard_should_draw_the_card_when_pile_has_only_one_card(t *testing.T) {
+	as := NewCard("as")
+	pile := deck.NewPile(as)
+
+	card, err := pile.DrawCard()
+	require.NoError(t, err)
+	assert.Equal(t, *card, as)
+}
+
+func TestDrawCard_should_return_error_when_draw_second_card_when_pile_has_only_one_card(t *testing.T) {
+	as := NewCard("as")
+	pile := deck.NewPile(as)
+
+	_, _ = pile.DrawCard()
+	_, err := pile.DrawCard()
+
+	assert.ErrorIs(t, err, deck.NoMoreCardsInThePile)
+}
+
+func NewCard(name string) deck.Card {
+	return &CardStub{name}
+}
+
+type CardStub struct {
+	name string
+}
+
+func (a CardStub) String() string {
+	return a.name
 }
