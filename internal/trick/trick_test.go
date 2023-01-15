f2ede2e0e2b181e6016ede70c165f5c4742f225a
Carlos Castillo <castillo.st@gmail.com>
2023-01-15 12:30:18 +0100

    ♻️ Refactor pile test to do not depend on string pile

--- internal/trick/trick_test.go
+++ internal/trick/trick_test.go
@@ -1,6 +1,8 @@
 package trick_test
 
 import (
+	"github.com/carloscasalar/card-guess/internal/deck"
+	"strings"
 	"testing"
 
 	"github.com/carloscasalar/card-guess/pkg/threepilestrick"
@@ -27,14 +29,22 @@ func Test_NewTrick_should_provide_21_different_random_cards_each_time(t *testing
 func Test_NewTrick_should_provide_same_exact_cards_when_dont_shuffle_before_draw(t *testing.T) {
 	aTrick, _ := threepilestrick.New(false)
 
-	trickCardsStr := aTrick.Sample().String()
-	assert.Equal(t, " A[♠]   2[♠]   3[♠]   4[♠]   5[♠]   6[♠]   7[♠]   8[♠]   9[♠]  10[♠]   J[♠]   Q[♠]   K[♠]   A[♥]   2[♥]   3[♥]   4[♥]   5[♥]   6[♥]   7[♥]   8[♥]", trickCardsStr)
+	trickCardsStr := cardsInPile(aTrick.Sample())
+	assert.Equal(t, " A[♠]  2[♠]  3[♠]  4[♠]  5[♠]  6[♠]  7[♠]  8[♠]  9[♠] 10[♠]  J[♠]  Q[♠]  K[♠]  A[♥]  2[♥]  3[♥]  4[♥]  5[♥]  6[♥]  7[♥]  8[♥]", trickCardsStr)
 }
 
 func Test_NewTrick_should_contain_a_mat_with_initial_sample_split_into_three_piles(t *testing.T) {
 	aTrick, _ := threepilestrick.New(false)
 
-	assert.Equal(t, " 6[♥]   3[♥]   K[♠]  10[♠]   7[♠]   4[♠]   A[♠]", aTrick.Mat().FirstPile().String())
-	assert.Equal(t, " 7[♥]   4[♥]   A[♥]   J[♠]   8[♠]   5[♠]   2[♠]", aTrick.Mat().SecondPile().String())
-	assert.Equal(t, " 8[♥]   5[♥]   2[♥]   Q[♠]   9[♠]   6[♠]   3[♠]", aTrick.Mat().ThirdPile().String())
+	assert.Equal(t, " 6[♥]  3[♥]  K[♠] 10[♠]  7[♠]  4[♠]  A[♠]", cardsInPile(aTrick.Mat().FirstPile()))
+	assert.Equal(t, " 7[♥]  4[♥]  A[♥]  J[♠]  8[♠]  5[♠]  2[♠]", cardsInPile(aTrick.Mat().SecondPile()))
+	assert.Equal(t, " 8[♥]  5[♥]  2[♥]  Q[♠]  9[♠]  6[♠]  3[♠]", cardsInPile(aTrick.Mat().ThirdPile()))
+}
+
+func cardsInPile(pile deck.Pile) string {
+	var cardStrings []string
+	for _, card := range pile.Cards() {
+		cardStrings = append(cardStrings, card.String())
+	}
+	return strings.Join(cardStrings, " ")
 }
